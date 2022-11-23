8ecf9e4bafb22786460d8938e9f68f728006e9f6
timoleon <tilamprinos@gmail.com>
2022-11-23 15:56:47 +0200

    Initial commit

---
+++ app/src/main/java/com/lamti/myapplication/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.lamti.myapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Green = Color(0xFF017E72)
+val WhiteTransparent = Color(0x33FFFFFF)
+val BlackTransparent = Color(0x33000000)
