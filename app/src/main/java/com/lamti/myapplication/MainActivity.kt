e8e292a523b8114390ac2ead2759227bec53b654
timoleon <tilamprinos@gmail.com>
2022-11-27 16:42:33 +0200

    set status bar color

--- app/src/main/java/com/lamti/myapplication/MainActivity.kt
+++ app/src/main/java/com/lamti/myapplication/MainActivity.kt
@@ -3,10 +3,13 @@ package com.lamti.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.lamti.myapplication.ui.navigation.PokedexApp
 import com.lamti.myapplication.ui.theme.PokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,11 +20,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
+                val systemUiController = rememberSystemUiController()
+                val useDarkIcons = !isSystemInDarkTheme()
+                val initialColor = MaterialTheme.colors.background
+                var statusBarColor by remember { mutableStateOf(initialColor) }
+
+                DisposableEffect(systemUiController, useDarkIcons, statusBarColor) {
+                    systemUiController.setStatusBarColor(
+                        color = statusBarColor,
+                        darkIcons = useDarkIcons
+                    )
+                    onDispose {}
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    PokedexApp()
+                    PokedexApp { color ->
+                        statusBarColor = color
+                    }
                 }
             }
         }
--- app/src/main/java/com/lamti/myapplication/ui/navigation/PokedexApp.kt
+++ app/src/main/java/com/lamti/myapplication/ui/navigation/PokedexApp.kt
@@ -1,15 +1,22 @@
 package com.lamti.myapplication.ui.navigation
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
 
 @Composable
-fun PokedexApp(modifier: Modifier = Modifier, startDestination: String = homeRoute) {
+fun PokedexApp(
+    modifier: Modifier = Modifier,
+    startDestination: String = homeRoute,
+    statusBarColor: (Color) -> Unit,
+) {
     val navController = rememberNavController()
+    val initialColor = MaterialTheme.colors.background
 
     NavHost(
         navController = navController,
@@ -23,11 +30,13 @@ fun PokedexApp(modifier: Modifier = Modifier, startDestination: String = homeRou
                     launchSingleTop = true
                     restoreState = true
                 }
+                statusBarColor(Color(color))
                 navController.navigateToDetails(code.toString(), color, navOptions)
             }
         )
         detailsScreen(
             onBackClick = {
+                statusBarColor(initialColor)
                 navController.popBackStack()
             }
         )
